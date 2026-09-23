19e812ed580b033eb5b063af4b7ea3e73519eb5b
Naman <iwillnotgive49@gmail.com>
2026-09-23 19:09:49 +0530

    Ready or Deployment 2

--- pages_app/settings.py
+++ pages_app/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+7$_!7i61$ba_%t3&rm7i4k8cawyjyx5b30x42d^p7vk4-5r5*"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
